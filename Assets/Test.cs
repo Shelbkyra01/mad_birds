4acd356be23bc453791c5ed38f2061e626ffca2e
shane moore <shane@homelink.co>
2021-07-02 15:47:42 +0000

    Initial check-in

---
+++ Assets/Test.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private GameObject _cloudParticlePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+    
+        if (col.gameObject.tag == "Floor") 
+            {
+            Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+            Destroy (gameObject);
+            }
+         
+    }
+
+}
